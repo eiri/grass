11ae9bfa1b98bce34cdce8291bd60c5da65d5df8
Vitaliy Avdeyev <eiri@eiri.ca>
2012-10-11 23:38:02 -0700

    Added basic CRUD operations on leveldb

--- src/grass.erl
+++ src/grass.erl
@@ -5,24 +5,54 @@
 -behaviour(application).
 -behaviour(supervisor).
 
+-define(WORKER(M), {M, {M, start_link, []}, permanent, 2000, worker, [M]}).
 -define(WORKER(M, A), {M, {M, start_link, [A]}, permanent, 2000, worker, [M]}).
+-define(STORAGE, gs_leveldb_server).
 
 %% pub API
--export([start/0, stop/0]).
+-export([start/0, stop/0, stats/0, bootstrap/0, all/0, get/1, put/2, delete/1]).
 %% behaviours callbacks
 -export([start/2, stop/1, init/1]).
+
+%% API
+
+stats() ->
+  lager:info("~n~s", [gen_server:call(?STORAGE, stats)]).
+
+bootstrap() ->
+  Add = fun(A, B) ->
+    Key = list_to_binary(io_lib:format("~s~s", [A, B])),
+    Value = list_to_binary(io_lib:format("~s~s", [B, A])),
+    gen_server:cast(?STORAGE, {put, Key, Value})
+  end,
+  AZ = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z],
+  [ Add(A, B) || A <- AZ, B <- AZ ].
+
+
+all() ->
+  gen_server:call(?STORAGE, all).
+
+get(Key) ->
+  gen_server:call(?STORAGE, {get, atom_to_binary(Key, latin1)}).
+
+put(Key, Value) ->
+  gen_server:cast(?STORAGE,
+      {put, atom_to_binary(Key, latin1), atom_to_binary(Value, latin1)}).
+
+delete(Key) ->
+  gen_server:cast(?STORAGE, {delete, atom_to_binary(Key, latin1)}).
 
 %% start/stop
 
 start() ->
   Fmt = "* Starting ~p: [~p]~n",
   Deps = [lager, crypto, inets, mochiweb, webmachine, grass],
-  [ io:format(Fmt, [App, application:start(App)]) || App <- Deps ].
+  [ error_logger:info_msg(Fmt, [App, application:start(App)]) || App <- Deps ].
 
 stop() ->
   Fmt = "* Stopping ~p: [~p]~n",
   Deps = [lager, crypto, inets, mochiweb, webmachine, grass],
-  [ io:format(Fmt, [App, application:stop(App)]) || App <- lists:reverse(Deps) ].
+  [ error_logger:info_msg(Fmt, [App, application:stop(App)]) || App <- lists:reverse(Deps) ].
 
 start(_Type, _StartArgs) ->
   supervisor:start_link({local, ?MODULE}, ?MODULE, []).
@@ -38,6 +68,7 @@ init([]) ->
   %% in dispatcher '*' is atom, even when first argument is a string!
   WebConfig = [{ip, "0.0.0.0"}, {port, Port}, {dispatch, [{['*'], gs_web, []}]}],
   Web = {web, {webmachine_mochiweb, start, [WebConfig]}, permanent, 2000, worker, dynamic},
-  LevelDB = ?WORKER(gs_leveldb_server, []),
+  WorkDir = "test.db", %% FIXME! move to app.config
+  LevelDB = ?WORKER(gs_leveldb_server, [WorkDir]),
   Children = [Web, LevelDB],
   {ok, {RestartStrategy, Children}}.
--- src/gs_leveldb_server.erl
+++ src/gs_leveldb_server.erl
@@ -5,25 +5,46 @@
 -export([start_link/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--record(ctx, {}).
+-record(ctx, {ref}).
 
 start_link(Args) ->
-  gen_server:start_link(?MODULE, Args, []).
+  gen_server:start_link({local, ?MODULE}, ?MODULE, Args, []).
 
-init(_Args) ->
-  {ok, #ctx{}}.
+init([WorkDir]) ->
+  case eleveldb:open(WorkDir, [{create_if_missing, true}]) of
+    {ok, Ref} ->
+      {ok, #ctx{ref = Ref}};
+    {error, Reason} ->
+      {error, Reason}
+  end.
 
-handle_call(_Msg, _From, State) ->
-  {reply, ok, State}.
+handle_call(all, _From, #ctx{ref = Ref} = Ctx) ->
+  Fun = fun({K, V}, Acc) ->
+    [{K, V}] ++ Acc
+  end,
+  All = eleveldb:fold(Ref, Fun, [], []),
+  {reply, All, Ctx};
+handle_call({get, Key}, _From, #ctx{ref = Ref} = Ctx) ->
+  Value = eleveldb:get(Ref, Key, []),
+  {reply, Value, Ctx};
+handle_call(stats, _From, #ctx{ref = Ref} = Ctx) ->
+  {ok, Stats} = eleveldb:status(Ref, <<"leveldb.stats">>),
+  {reply, Stats, Ctx}.
 
-handle_cast(_Msg, State) ->
-  {noreply, State}.
+handle_cast({put, Key, Value}, #ctx{ref = Ref} = Ctx) ->
+  ok = eleveldb:put(Ref, Key, Value, []),
+  {noreply, Ctx};
+handle_cast({delete, Key}, #ctx{ref = Ref} = Ctx) ->
+  ok = eleveldb:delete(Ref, Key, []),
+  {noreply, Ctx}.
 
-handle_info(_Msg, State) ->
-  {noreply, State}.
+handle_info(Msg, Ctx) ->
+  lager:info("got ~p", [Msg]),
+  {noreply, Ctx}.
 
-terminate(_Reason, _State) ->
+terminate(_Reason, #ctx{ref = Ref}) ->
+  eleveldb:close(Ref),
   ok.
 
-code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
+code_change(_OldVsn, Ctx, _Extra) ->
+  {ok, Ctx}.
